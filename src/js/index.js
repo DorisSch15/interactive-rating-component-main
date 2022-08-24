016b01c608f54282d33d16273e3a67ec28fd8785
Doris <sayitloud15@gmail.com>
2022-08-24 20:24:49 +0200

    adding js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,25 @@
 import '../assets/styles/scss/main.scss';
+
+let btnSubmit = document.querySelector('.choose__btn');
+let selection = document.querySelector('.choose__selection');
+let numberSelection = document.querySelector('.choose__selection-n');
+let thankYouBox = document.querySelector('.thankyou');
+let chooseBox = document.querySelector('.choose');
+let choosenNumber = document.querySelector('.choosenNumber');
+let number
+
+selection.addEventListener('click', zahlAusgeben);
+btnSubmit.addEventListener('click', showThankYou)
+
+function zahlAusgeben(e) {
+    if (e.target && e.target.matches('div')){
+        number = e.target.innerText;
+        e.target.classList.add('activ');
+    };
+};
+
+function showThankYou(e){
+    thankYouBox.classList.remove('hidden');
+    chooseBox.classList.add('hidden');
+    choosenNumber.innerHTML = `&nbsp;${number}&nbsp;`;
+};
